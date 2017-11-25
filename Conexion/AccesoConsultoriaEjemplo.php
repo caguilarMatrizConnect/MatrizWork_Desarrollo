14a645cc3ecffec9ac829e62fb4a4fdaff3b6864
Carlos <Carlos@localhost>
2017-11-25 12:04:53 +0100

    2017/11/23: Creación de desarrollo de conexión

---
+++ Conexion/AccesoConsultoriaEjemplo.php
@@ -0,0 +1,20 @@
+<?php
+
+   $serverName = "WIN-OUVEI4V1R4S\MATRIZWORK";   
+   $database = "ConsultoriaEjemplo";  
+
+   // Get UID and PWD from application-specific files.   
+   $uid = file_get_contents("..\Credenciales\UIX_ConsultoriaEjemplo_Lectura.txt");  
+   $pwd = file_get_contents("..\Credenciales\PWD_ConsultoriaEjemplo_Lectura.txt");
+
+   try {  
+      $conn = new PDO( "sqlsrv:server=$serverName;Database = $database", $uid, $pwd);   
+      $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );   
+   }  
+
+   catch( PDOException $e ) {  
+      die( "Error conectando a su base de datos" );   
+   }  
+
+ 
+?>
---
+++ Conexion/AccesoMatrizWork.php
@@ -0,0 +1,20 @@
+<?php
+
+   $serverName = "WIN-OUVEI4V1R4S\MATRIZWORK";   
+   $database = "MatrizWork_Desarrollo";  
+
+   // Get UID and PWD from application-specific files.   
+   $uid = file_get_contents("..\Credenciales\UIX_Acceso.txt");  
+   $pwd = file_get_contents("..\Credenciales\PWD_Acceso.txt");  
+
+   try {  
+      $conn = new PDO( "sqlsrv:server=$serverName;Database = $database;Encrypt = on", $uid, $pwd);   
+      $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );   
+   }  
+
+   catch( PDOException $e ) {  
+      die( "Error conectando a su base de datos" );   
+   }  
+
+ 
+?>
